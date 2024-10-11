ba6dc49675e459af426df85724c6cb2dc9c180e0
Nick Chan <nick.chan@utexas.edu>
2024-10-11 15:36:52 -0500

    Finalized path

--- visual_representation_learning/launch/record_rosbag.launch.py
+++ visual_representation_learning/launch/record_rosbag.launch.py
@@ -2,20 +2,8 @@
 """
 record_rosbag.launch.py
 
-This launch file is used to record ROS2 topics into a rosbag file. The topics to be recorded
-are specified in a YAML configuration file. The recorded bag files are stored in a directory
-with a timestamped filename to avoid overwriting. The launch file includes functionality to
-declare launch arguments, execute the recording process, and log information about the recording.
-
-Functions:
-- load_topics: Loads the list of topics to be recorded from a YAML configuration file.
-- get_file_name: Generates a timestamped filename for the rosbag and ensures the directory exists.
-- launch_setup: Sets up the recording process and logs relevant information.
-- generate_launch_description: Declares launch arguments and returns the launch description.
-
-Usage:
-- The topics to be recorded should be specified in 'record_rosbag.yaml' located in the config directory.
-- The recorded bag files will be stored in the 'bags' directory with a timestamped filename.
+This script sets up and launches a ROS2 process to record specified topics into a rosbag file.
+The configuration for the topics to be recorded is loaded from a YAML file.
 """
 
 import os
@@ -32,36 +20,31 @@ from launch.actions import (
     RegisterEventHandler,
 )
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 
 package_share_directory = get_package_share_directory("visual_representation_learning")
 
-
-def load_topics():
-    # Load the default topics from the YAML file
-    config_dir = os.path.join(package_share_directory, "config")
-    config_file = os.path.join(config_dir, "rosbag.yaml")
-    with open(config_file, "r") as file:
-        config = yaml.safe_load(file)
-    return config["recorded_topics"]
+# Load the configuration from the YAML file
+with open(os.path.join(package_share_directory, "config", "rosbag.yaml"), "r") as file:
+    config = yaml.safe_load(file)
 
 
 def launch_setup(context, *args, **kwargs):
-    topics_list = load_topics()
+    topics_list = config["recorded_topics"]
     bag_name = LaunchConfiguration("bag_name").perform(context)
     output_dir = LaunchConfiguration("output_dir").perform(context)
+    bag_file_path = os.path.join(output_dir, bag_name)
+
+    # Ensure the output directory exists
     os.makedirs(output_dir, exist_ok=True)
+
+    # Check if the bag file already exists
+    if os.path.exists(bag_file_path):
+        raise FileExistsError(f"The bag file '{bag_file_path}' already exists.")
 
     # Define the process to record the bag
     record_bag_process = ExecuteProcess(
-        cmd=[
-            "ros2",
-            "bag",
-            "record",
-            "-o",
-            PathJoinSubstitution([output_dir, bag_name]),
-        ]
-        + topics_list,
+        cmd=["ros2", "bag", "record", "-o", bag_file_path] + topics_list,
         output="screen",
     )
 
@@ -87,20 +70,18 @@ def launch_setup(context, *args, **kwargs):
 def generate_launch_description():
     declared_arguments = []
 
-    default_bag_name = datetime.now().strftime("%Y-%m-%d-%H-%M-%S") + ".bag"
     declared_arguments.append(
         DeclareLaunchArgument(
             "bag_name",
-            default_value=default_bag_name,
+            default_value=f'vrl_data_{datetime.now().strftime("%Y%m%d_%H%M%S")}',
             description="Name of the bag file to save the recording",
         ),
     )
 
-    default_output_dir = os.path.expanduser("~/bags/utexas_sterling")
     declared_arguments.append(
         DeclareLaunchArgument(
             "output_dir",
-            default_value=default_output_dir,
+            default_value=os.path.expanduser("~/bags/utexas_sterling"),
             description="Directory to store generated bag files",
         ),
     )
