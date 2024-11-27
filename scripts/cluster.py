54d1ecd7690ca2f52c7edd5e7c956cf722279a4c
Justin Hart. <hart@cs.utexas.edu>
2024-11-27 15:02:23 -0600

    Added a basic implementation of k-means clustering with no frills.

--- scripts/cluster.py
+++ scripts/cluster.py
@@ -24,9 +24,42 @@ if __name__ == "__main__":
     batch = next(iter(dataloader))
     patch1, _ = batch
 
-    # patch1 = torch.cat([data[0] for data in dataloader])
-    # patch2 = torch.cat([data[1] for data in dataloader])
-    print("long_tensor.shape:  ", patch1.shape)
-    # patch1 = patch1[0]
     patch1 = patch1.to(device)
     representation_vectors = model.visual_encoder(patch1)
+
+    print("long_tensor.shape:  ", patch1.shape)
+    print("representation_vectors.shape:  ", representation_vectors.shape)
+
+    # K Means
+    k = 5
+    iterations = 2
+    centroids = representation_vectors[torch.randperm(representation_vectors.size(0))[:k]]
+
+    for _ in range(iterations):
+        distances = torch.cdist(representation_vectors, centroids)
+        clusters = torch.argmin(distances, dim=1)
+        new_centroids = torch.stack([representation_vectors[clusters == i].mean(dim=0) for i in range(k)])
+
+        if torch.allclose(centroids, new_centroids):
+            break
+
+    print("I made (k) clusters: ", k)
+    print("Number of items in each cluster.")
+    for i in range(0,k):
+        print(" [", i, "]: ", representation_vectors[clusters == i].shape)
+    
+    #Find the 5 farthest apart vectors for each cluster
+    for i in range(0,k):
+        cluster = representation_vectors[clusters == i]
+        row_norms = torch.norm(representation_vectors, dim=1, keepdim=True)
+        normalized_tensor = representation_vectors / row_norms
+
+
+    '''
+    TO DO:
+    Find the 5 farthest apart vectors for each cluster
+    Index them from the bigger representation vectors
+    Get the indices where they occur in patch1
+    Render the patches from patch1 corresponding to each cluster to get a representative sample
+        of each cluster    
+    '''
