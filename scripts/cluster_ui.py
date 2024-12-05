91820cc85eaa550d496ca3f2289516abd97923b5
Nick Chan <nick.chan@utexas.edu>
2024-12-05 16:01:32 -0600

    Refactored select components to class

--- scripts/cluster_ui.py
+++ scripts/cluster_ui.py
@@ -23,16 +23,17 @@ class ClusterUI(Gtk.Application):
         vbox.set_margin_end(10)
         window.set_child(vbox)
 
-        # Add a text entry box
-        vbox.append(Gtk.Label(label="Data pickle file selected:"))
+        ### Pickle file selector
+        spf = SelectPickleFile(window)
+        vbox.append(spf.get_component())
+
+        # Add a spacer
+        spacer = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
+        vbox.append(spacer)
         
-        self.data_pkl_label = Gtk.Label(label="None")
-        vbox.append(self.data_pkl_label)
-        
-        # Add a file chooser button
-        file_chooser_button = Gtk.Button(label="Select a File")
-        file_chooser_button.connect("clicked", self.on_file_chooser_button_clicked)
-        vbox.append(file_chooser_button)
+        smf = SelectModelFile(window)
+        vbox.append(smf.get_component())
+
 
         # Add another text entry box
         entry2 = Gtk.Entry()
@@ -51,31 +52,6 @@ class ClusterUI(Gtk.Application):
 
         # Show the window
         window.present()
-        
-    def on_file_chooser_button_clicked(self, button):
-        dialog = Gtk.FileChooserDialog(
-            title="Select data file",
-            transient_for=self.get_active_window(),
-            action=Gtk.FileChooserAction.OPEN,
-        )
-        dialog.add_buttons(
-            "_Cancel", Gtk.ResponseType.CANCEL,
-            "_Open", Gtk.ResponseType.ACCEPT
-        )
-        dialog.connect("response", self.on_file_chooser_response)
-        dialog.show()
-
-    def on_file_chooser_response(self, dialog, response):
-        if response == Gtk.ResponseType.ACCEPT:
-            file_path = dialog.get_file().get_path()
-            if not file_path.endswith('.pkl'):
-                self.data_pkl_label.set_markup("<span foreground='red'>Error: Selected file is not a .pkl file</span>")
-                dialog.destroy()
-                return
-            
-            self.data_pkl = file_path
-            self.data_pkl_label.set_text(self.data_pkl)
-        dialog.destroy()
 
     def on_button1_clicked(self, button):
         print("Button 1 clicked!")
@@ -83,6 +59,103 @@ class ClusterUI(Gtk.Application):
     def on_button2_clicked(self, button):
         print("Button 2 clicked!")
 
+class SelectPickleFile:
+    def __init__(self, parent_window):
+        self.parent_window = parent_window
+        self.data_pkl_path = None
+
+    def get_component(self):
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        vbox.set_margin_top(10)
+        vbox.set_margin_bottom(10)
+        vbox.set_margin_start(10)
+        vbox.set_margin_end(10)
+
+        vbox.append(Gtk.Label(label="Data pickle file selected:"))
+
+        # Pickle file status
+        self.label = Gtk.Label(label="None")
+        vbox.append(self.label)
+
+        # Pickle file chooser button
+        file_chooser_button = Gtk.Button(label="Open")
+        file_chooser_button.connect("clicked", self.on_file_chooser_button_clicked)
+        vbox.append(file_chooser_button)
+        
+        return vbox
+
+    def on_file_chooser_button_clicked(self, button):
+        dialog = Gtk.FileChooserDialog(
+            title="Select Data Pickle File",
+            transient_for=self.parent_window,
+            action=Gtk.FileChooserAction.OPEN,
+        )
+        dialog.add_buttons("_Cancel", Gtk.ResponseType.CANCEL, "_Open", Gtk.ResponseType.ACCEPT)
+        dialog.connect("response", self.on_file_chooser_response)
+        dialog.show()
+
+    def on_file_chooser_response(self, dialog, response):
+        if response == Gtk.ResponseType.ACCEPT:
+            file_path = dialog.get_file().get_path()
+            if not file_path.endswith(".pkl"):
+                self.label.set_markup(
+                    "<span foreground='red'>Error: Selected file is not a .pkl file</span>"
+                )
+                dialog.destroy()
+                return
+
+            self.data_pkl_path = file_path
+            self.label.set_markup(f"<span foreground='green'>{self.data_pkl_path}</span>")
+        dialog.destroy()
+
+class SelectModelFile:
+    def __init__(self, parent_window):
+        self.parent_window = parent_window
+        self.model_path = None
+
+    def get_component(self):
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        vbox.set_margin_top(10)
+        vbox.set_margin_bottom(10)
+        vbox.set_margin_start(10)
+        vbox.set_margin_end(10)
+
+        vbox.append(Gtk.Label(label="Model file selected:"))
+
+        # Pickle file status
+        self.label = Gtk.Label(label="None")
+        vbox.append(self.label)
+
+        # Pickle file chooser button
+        file_chooser_button = Gtk.Button(label="Open")
+        file_chooser_button.connect("clicked", self.on_file_chooser_button_clicked)
+        vbox.append(file_chooser_button)
+        
+        return vbox
+
+    def on_file_chooser_button_clicked(self, button):
+        dialog = Gtk.FileChooserDialog(
+            title="Select PyTorch Model File",
+            transient_for=self.parent_window,
+            action=Gtk.FileChooserAction.OPEN,
+        )
+        dialog.add_buttons("_Cancel", Gtk.ResponseType.CANCEL, "_Open", Gtk.ResponseType.ACCEPT)
+        dialog.connect("response", self.on_file_chooser_response)
+        dialog.show()
+
+    def on_file_chooser_response(self, dialog, response):
+        if response == Gtk.ResponseType.ACCEPT:
+            file_path = dialog.get_file().get_path()
+            if not file_path.endswith(".pt"):
+                self.label.set_markup(
+                    "<span foreground='red'>Error: Selected file is not a .pt file</span>"
+                )
+                dialog.destroy()
+                return
+
+            self.model_path = file_path
+            self.label.set_markup(f"<span foreground='green'>{self.model_path}</span>")
+        dialog.destroy()
 
 app = ClusterUI()
 exit_status = app.run(sys.argv)
