91b92c20a73b3040da7836587b5446281eae9c5d
Nick Chan <nick.chan@utexas.edu>
2024-12-05 17:07:53 -0600

    Shows clusters

--- scripts/cluster_ui.py
+++ scripts/cluster_ui.py
@@ -1,7 +1,8 @@
 import sys
 
 import gi
-from gi.repository import GLib, Gtk
+from gi.repository import GLib, Gtk, GdkPixbuf
+from cluster import Cluster, PatchRenderer
 
 gi.require_version("Gtk", "4.0")
 
@@ -23,41 +24,22 @@ class ClusterUI(Gtk.Application):
         vbox.set_margin_end(10)
         window.set_child(vbox)
 
-        ### Pickle file selector
         spf = SelectPickleFile(window)
         vbox.append(spf.get_component())
 
-        # Add a spacer
-        spacer = Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL)
-        vbox.append(spacer)
-        
+        vbox.append(Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL))
+
         smf = SelectModelFile(window)
         vbox.append(smf.get_component())
 
+        vbox.append(Gtk.Separator(orientation=Gtk.Orientation.HORIZONTAL))
 
-        # Add another text entry box
-        entry2 = Gtk.Entry()
-        entry2.set_placeholder_text("Enter more text here...")
-        vbox.append(entry2)
-
-        # Add the first button
-        button1 = Gtk.Button(label="Button 1")
-        button1.connect("clicked", self.on_button1_clicked)
-        vbox.append(button1)
-
-        # Add the second button
-        button2 = Gtk.Button(label="Button 2")
-        button2.connect("clicked", self.on_button2_clicked)
-        vbox.append(button2)
+        gc = GenerateClusters(window, spf, smf)
+        vbox.append(gc.get_component())
 
         # Show the window
         window.present()
 
-    def on_button1_clicked(self, button):
-        print("Button 1 clicked!")
-
-    def on_button2_clicked(self, button):
-        print("Button 2 clicked!")
 
 class SelectPickleFile:
     def __init__(self, parent_window):
@@ -81,7 +63,7 @@ class SelectPickleFile:
         file_chooser_button = Gtk.Button(label="Open")
         file_chooser_button.connect("clicked", self.on_file_chooser_button_clicked)
         vbox.append(file_chooser_button)
-        
+
         return vbox
 
     def on_file_chooser_button_clicked(self, button):
@@ -98,15 +80,14 @@ class SelectPickleFile:
         if response == Gtk.ResponseType.ACCEPT:
             file_path = dialog.get_file().get_path()
             if not file_path.endswith(".pkl"):
-                self.label.set_markup(
-                    "<span foreground='red'>Error: Selected file is not a .pkl file</span>"
-                )
+                self.label.set_markup("<span foreground='red'>Error: Selected file is not a .pkl file</span>")
                 dialog.destroy()
                 return
 
             self.data_pkl_path = file_path
             self.label.set_markup(f"<span foreground='green'>{self.data_pkl_path}</span>")
         dialog.destroy()
+
 
 class SelectModelFile:
     def __init__(self, parent_window):
@@ -130,7 +111,7 @@ class SelectModelFile:
         file_chooser_button = Gtk.Button(label="Open")
         file_chooser_button.connect("clicked", self.on_file_chooser_button_clicked)
         vbox.append(file_chooser_button)
-        
+
         return vbox
 
     def on_file_chooser_button_clicked(self, button):
@@ -147,15 +128,132 @@ class SelectModelFile:
         if response == Gtk.ResponseType.ACCEPT:
             file_path = dialog.get_file().get_path()
             if not file_path.endswith(".pt"):
-                self.label.set_markup(
-                    "<span foreground='red'>Error: Selected file is not a .pt file</span>"
-                )
+                self.label.set_markup("<span foreground='red'>Error: Selected file is not a .pt file</span>")
                 dialog.destroy()
                 return
 
             self.model_path = file_path
             self.label.set_markup(f"<span foreground='green'>{self.model_path}</span>")
         dialog.destroy()
+
+
+class GenerateClusters:
+    def __init__(self, parent_window, spf, smf):
+        self.parent_window = parent_window
+        self.spf = spf
+        self.smf = smf
+
+        # self.vbox
+        # self.entry_clusters
+        # self.entry_iterations
+
+    def get_component(self):
+        self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.vbox.set_margin_top(10)
+        self.vbox.set_margin_bottom(10)
+        self.vbox.set_margin_start(10)
+        self.vbox.set_margin_end(10)
+
+        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+
+        label = Gtk.Label(label="Number of Clusters:")
+        hbox.append(label)
+
+        self.entry_clusters = Gtk.Entry()
+        self.entry_clusters.set_placeholder_text("Enter number of clusters...")
+        hbox.append(self.entry_clusters)
+
+        self.vbox.append(hbox)
+
+        hbox_iterations = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+
+        label_iterations = Gtk.Label(label="Number of Iterations:")
+        hbox_iterations.append(label_iterations)
+
+        self.entry_iterations = Gtk.Entry()
+        self.entry_iterations.set_placeholder_text("Enter number of iterations...")
+        hbox_iterations.append(self.entry_iterations)
+
+        self.vbox.append(hbox_iterations)
+
+        button = Gtk.Button(label="Generate Clusters")
+        button.connect("clicked", self.on_button_clicked)
+        self.vbox.append(button)
+
+        return self.vbox
+
+    def on_button_clicked(self, button):
+        data_pkl_path = self.spf.data_pkl_path
+        model_path = self.smf.model_path
+
+        if data_pkl_path is None or model_path is None:
+            error_dialog = Gtk.MessageDialog(
+                transient_for=self.parent_window,
+                modal=True,
+                message_type=Gtk.MessageType.ERROR,
+                buttons=Gtk.ButtonsType.OK,
+                text="Please select both a data pickle file and a model file before generating clusters.",
+            )
+            error_dialog.show()
+            error_dialog.connect("response", lambda dialog, response: dialog.destroy())
+            return
+
+        num_clusters = self.entry_clusters.get_text()
+        num_iterations = self.entry_iterations.get_text()
+
+        try:
+            num_clusters = int(num_clusters)
+            num_iterations = int(num_iterations)
+            if num_clusters <= 0 or num_iterations <= 0:
+                raise ValueError
+        except ValueError:
+            error_dialog = Gtk.MessageDialog(
+                transient_for=self.parent_window,
+                modal=True,
+                message_type=Gtk.MessageType.ERROR,
+                buttons=Gtk.ButtonsType.OK,
+                text="Please enter valid positive numbers for clusters and iterations.",
+            )
+            error_dialog.show()
+            error_dialog.connect("response", lambda dialog, response: dialog.destroy())
+            return
+
+        # Generate clusters
+        cluster = Cluster(data_pkl_path, model_path)
+        all_cluster_image_indices = cluster.generate_clusters(num_clusters, num_iterations)
+
+        # Render clusters
+        rendered_clusters = PatchRenderer.render_clusters(all_cluster_image_indices, cluster.patches)
+        
+        
+        images = []
+        for i, cluster in enumerate(rendered_clusters):
+            images.append(PatchRenderer.image_grid(cluster))
+            
+        def pil_to_pixbuf(pil_image):
+            data = pil_image.tobytes()
+            width, height = pil_image.size
+            return GdkPixbuf.Pixbuf.new_from_data(
+                data,
+                GdkPixbuf.Colorspace.RGB,
+                pil_image.mode == "RGBA",
+                8,
+                width,
+                height,
+                pil_image.mode == "RGBA" and width * 4 or width * 3,
+            )
+            
+            
+        hbox_images = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=10)
+            
+        for image in images:
+            pixbuf = pil_to_pixbuf(image)
+            image_widget = Gtk.Image.new_from_pixbuf(pixbuf)
+            image_widget.set_size_request(200, 200)  # Set the desired width and height
+            hbox_images.append(image_widget)
+            
+        self.vbox.append(hbox_images)
+
 
 app = ClusterUI()
 exit_status = app.run(sys.argv)
